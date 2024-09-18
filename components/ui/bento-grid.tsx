3e3eb554c1bda4fa865ee9bc853063c5d798f009
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2024-09-18 15:03:46 -0700

    add: globe.tsx and GridGlobe.tsx

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -1,4 +1,6 @@
 import { cn } from "@/lib/utils";
+import { BackgroundGradientAnimation } from "./background-gradient-animation";
+import { GlobeDemo } from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -66,19 +68,29 @@ export const BentoGridItem = ({
             <img
               src={spareImg}
               alt={spareImg}
-              className="object-cover, object-center, w-full, h-full"
+              className="object-cover object-center w-full h-full"
             />
           )}
         </div>
-        {id === }
-      </div>
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-          {title}
+        {id === 6 && (
+          <BackgroundGradientAnimation>
+            <div className="absolute z-50 flex items-center justify-center text-white font-bold"/>
+          </BackgroundGradientAnimation>
+        )}
+
+        <div className={cn(
+          titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10 bg-white dark:bg-black dark:bg-opacity-20 rounded-3xl shadow-input dark:shadow-none'
+        )}>
+          <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
+            {description}
+          </div>
+
+          <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+            {title}
+          </div>
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
-        </div>
+
+        {id === 2 && <GlobeDemo />}
       </div>
     </div>
   );
