87f181b5f65223262a8a00aa0c89cfb5697bfd15
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2024-09-19 14:40:48 -0700

    add: react-lottie and confetti.json

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./background-gradient-animation";
 import { GlobeDemo } from "./GridGlobe";
 import { div } from "three/webgpu";
+import Lottie from "react-lottie";
+import { useState } from "react";
+import animationData from "@/data/confetti.json";
 
 export const BentoGrid = ({
   className,
@@ -43,6 +48,9 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+
+  const [copied, setCopied] = useState(false);
+
   return (
     <div
       className={cn(
@@ -116,7 +124,14 @@ export const BentoGridItem = ({
         {id === 6 && (
           <div className="mt-5 relative">
             <div className={`absolute -bottom-5 right-0`}>
-              
+              <Lottie options={{
+                loop: copied,
+                autoplay: copied,
+                animationData,
+                rendererSettings: {
+                  preserveAspectRatio: 'xMidYMid slice'
+                }
+              }} />
             </div>
 
           </div>
