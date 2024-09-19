7ac9190c0dac86c2d1e8be0b49a2defb0d4ecd92
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2024-09-18 22:09:53 -0700

    random fix. whoops!

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -103,13 +103,22 @@ export const BentoGridItem = ({
               <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]"/>
             </div>
             <div className="flex flex-col gap-3 lg:gap-8">
-              {['MongoDB', 'Python', 'CMS'].map((item) => (
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]"/>
+              {['MongoDB', 'Python', 'CMS', 'GraphQL'].map((item) => (
                 <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                   {item}
                 </span>
               ))}
-              <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]"/>
             </div>
+          </div>
+        )}
+
+        {id === 6 && (
+          <div className="mt-5 relative">
+            <div className={`absolute -bottom-5 right-0`}>
+              
+            </div>
+
           </div>
         )}
       </div>
