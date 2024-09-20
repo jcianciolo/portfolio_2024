b00023a8c742081b1f279aa2065b32842fcfb187
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2024-09-19 17:11:12 -0700

    add: 3d-pin.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import { projects } from '@/data/index'
 
+import dynamic from "next/dynamic";
+const PinContainer = dynamic(
+
+  () => import("./ui/3d-pin").then((mod) => mod.PinContainer),
+
+  { ssr: false }
+
+);
+
 const RecentProjects = () => {
   return (
     <div className="py-20">
@@ -11,7 +20,9 @@ const RecentProjects = () => {
         <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
             {projects.map(({ id, title, des, img, iconLists, link }) => (
                 <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[90vw]">
-                    {title}
+                    <PinContainer>
+                        {title}
+                    </PinContainer>
                 </div>
             ))}
         </div>
