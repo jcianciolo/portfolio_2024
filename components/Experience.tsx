e2a0c7cbadbe8d5e207195a4808d3b28110279bf
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2024-09-23 17:05:25 -0700

    add: moving-border.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,8 +1,25 @@
+import { workExperience } from '@/data'
 import React from 'react'
+import { Button } from './ui/moving-border'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div className="py-20" id="testimonials">
+        <h1 className="heading">
+            My
+            <span className="text-purple"> work experience</span>
+        </h1>
+
+        <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10S">
+          {workExperience.map((card) => (
+              <Button>
+                <div>
+                  <img src={card.thumbnail} alt={card.thumbnail} className="lg:w-32 md:w-20 w-16"/>
+                </div>
+              </Button>
+          ))}
+        </div>
+    </div>
   )
 }
 
